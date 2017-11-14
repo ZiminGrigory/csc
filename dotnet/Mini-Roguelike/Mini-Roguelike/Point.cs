b3c27dcb9faf4444622bf67f4c1010264459e3dc
greg <lahmatbiy@gmail.com>
2017-11-15 01:43:27 +0300

    TESTS ADDED
    Signed-off-by: greg <lahmatbiy@gmail.com>

--- dotnet/Mini-Roguelike/Mini-Roguelike/Point.cs
+++ dotnet/Mini-Roguelike/Mini-Roguelike/Point.cs
@@ -4,5 +4,24 @@
     {
         public int X { get; set; }
         public int Y { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var oth = (Point) obj;
+            return oth != null && Equals(oth);
+        }
+
+        protected bool Equals(Point other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
     }
 }
---
+++ dotnet/Mini-Roguelike/UnitTestRogueLike/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestRogueLike")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestRogueLike")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b3aab47-0e24-43fa-9df5-d2d2395213d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
